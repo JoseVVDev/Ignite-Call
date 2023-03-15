5007eaa2b16bfb51098458f749205d827a4b4f99
JoseVVDev <78755947+JoseVVDev@users.noreply.github.com>
2023-03-15 14:48:57 -0300

    Aulas 13 & 14/03

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,31 +4,39 @@ import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as="h1" size="4xl">
-          Agendamento descomplicado
-        </Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100} // next image por padrão usa 80% de qualidade, esse código e pra usar 100%
-          priority // Dá prioridade no carregamento da pagina para a imagem, por padrão não há prioridade
-          alt="Calendário simbolizando aplicação em funcionamento"
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
